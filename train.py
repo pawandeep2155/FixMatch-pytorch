317b1b9705cd9fc0b4e6e00e8cf3541c6c18ee70
Pawandeep Singh <pawandeep@madstreetden.com>
2021-04-29 12:12:17 +0530

    Custom Dataset temporary change

--- train.py
+++ train.py
@@ -199,14 +199,14 @@ def main():
             args.model_depth = 29
             args.model_width = 64
     elif args.dataset == 'filtered1500':
-        args.num_classes = 4
+        args.num_classes = 10
         if args.arch == 'wideresnet':
             args.model_depth = 28
-            args.model_width = 8
+            args.model_width = 2
         elif args.arch == 'resnext':
-            args.model_cardinality = 8
-            args.model_depth = 29
-            args.model_width = 64
+            args.model_cardinality = 4
+            args.model_depth = 28
+            args.model_width = 4
 
     if args.local_rank not in [-1, 0]:
         torch.distributed.barrier()
